9ee3a6d5e733c1e248e81bfc21514b0527da967b
mfumagalli <mfumagalli82@gmail.com>
2018-08-14 15:09:01 +0100

    fix NCD

--- scripts/samplingNCD.R
+++ scripts/samplingNCD.R
@@ -4,14 +4,15 @@ args <- commandArgs(T)
 if (length(args)==0) {
         stop("No arguments given.
         Aim: Estimate NCD statistics (Bittarello) by sampling from .saf files.
-        Usage: Rscript NCD.R input.saf.idx.gz nr_chroms target_freq 
-	Output is in stdout: nr_sites \t NCD
+        Usage: Rscript NCD.R input.saf.idx.gz nr_chroms target_freq
+	Output is in stdout: nr_sites1 \t NCD1 \t nr_sites2 \t NCD2
         Info: this program take unfolded .saf.idx.gz data.\n")
 }
 
 fin <- args[1] # saf.idx file, this must be unfolded
 nchroms <- as.numeric(args[2]) # how many chroms
-tf <- as.numeric(args[3])
+tf <- as.numeric(args[3]) # target balanced frequency
+type <- as.numeric(args[4]) # NCD1 NCD2
 rm(args)
 
 nl <- 1000000
@@ -33,13 +34,29 @@ while (TRUE) {
                 break
         }
         ss <- strsplit(mline, split="\t")
-        aaf <- c(aaf, sapply(ss, expsam)/nchroms)
+        aaf <- c(aaf, (sapply(ss, expsam)-1))
 }
 close(con)
 
-write(length(aaf), stderr())
+# fixed differences
+fd <- which(aaf==nchroms)
 
-write(sqrt(mean((aaf-tf)^2)), stdout())
+# snps
+snps <- which(aaf>0 & aaf<nchroms)
+
+# maf
+aaf <- aaf/nchroms
+aaf[which(aaf>0.5)] <- 1 - aaf[which(aaf>0.5)]
+
+# NCD1
+ind1 <- snps
+ncd1 <- sqrt(mean((aaf[ind1]-tf)^2))
+
+# NCD2
+ind2 <- sort(unique(snps,fd))
+ncd2 <- sqrt(mean((aaf[ind2]-tf)^2))
+
+cat(length(ind1),"\t",ncd1,"\t",length(ind2),"\t",ncd2,"\n")
 
 
 
